5b725940fd6e713ee1446ce650f7de8b35f7dfb7
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-06 03:05:13 +0530

    final quote added and modified

--- src/Quote.js
+++ src/Quote.js
@@ -9,6 +9,8 @@ const Quote = () =>{
                 <h1 data-shadow='dang!'>“Everybody in this <br /> country
                 should learn  <br />to program a computer,<br />
                 because it teaches <br /> you how to think”</h1>
+                <h2>STEVE JOBS</h2>
+
             </div>
 
         
