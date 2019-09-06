9ca000518775879983982d57b8ab3cbfb92dcc41
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-06 13:44:14 +0530

    image fixed

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ const Card = () =>{
     return(
         <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             
-            <img alt="nothing" src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png'></img>
+            <img className='course' alt="nothing" src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1024px-Python-logo-notext.svg.png'></img>
                 <h2>C++</h2>
                 <p>Data structures and algorithms,<br /> 
                     Data structures and algorithms, <br />
