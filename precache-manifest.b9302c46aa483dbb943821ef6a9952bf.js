e21cd60b9e8195e983ab5cbb9220dfb9e659e91d
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-06 16:39:45 +0530

    Updates

---
+++ precache-manifest.b9302c46aa483dbb943821ef6a9952bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b97d6c77b309bb5aa309d2d2c9c49b",
+    "url": "/techblocks/index.html"
+  },
+  {
+    "revision": "deb7be2f4af9e325a75d",
+    "url": "/techblocks/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "80ff71dbb11ca1b46e97",
+    "url": "/techblocks/static/css/main.43c47af0.chunk.css"
+  },
+  {
+    "revision": "deb7be2f4af9e325a75d",
+    "url": "/techblocks/static/js/2.32cdb58a.chunk.js"
+  },
+  {
+    "revision": "80ff71dbb11ca1b46e97",
+    "url": "/techblocks/static/js/main.ff755436.chunk.js"
+  },
+  {
+    "revision": "76eae31dba3ba3bdf758",
+    "url": "/techblocks/static/js/runtime~main.f2a4f8a5.js"
+  }
+]);
